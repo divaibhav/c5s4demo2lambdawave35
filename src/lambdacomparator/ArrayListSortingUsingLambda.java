ebb9d8ddab3b650ad8705dff8034741e511be705
Vaibhav Diwan <vaibhav.diwan@niit.com>
2022-11-05 11:17:25 +0530

    Initial commit

---
+++ src/lambdacomparator/ArrayListSortingUsingLambda.java
@@ -0,0 +1,2 @@
+package lambdacomparator;public class ArrayListSortingUsingLambda {
+}
---
+++ src/lambdacomparator/Student.java
@@ -0,0 +1,47 @@
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int rollNo;
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName, int rollNo) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.rollNo = rollNo;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", rollNo=" + rollNo +
+                '}';
+    }
+}
